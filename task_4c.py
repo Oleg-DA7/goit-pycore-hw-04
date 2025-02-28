baa89bf2632e4040ab50d0553e133e1c49a56c45
Shon <onshad@gmail.com>
2025-03-01 00:55:02 +0200

    v5

---
+++ task_4c.py
@@ -0,0 +1,64 @@
+class Contact():
+    def __init__(self, name, phone):
+        self.name = name
+        self.phone = phone 
+    def __str__(self):
+        return f'Contact name: {self.name}, phone: {self.phone}'
+
+class ContactList():
+    def __init__(self):
+        self.contacts = []    
+    def add_contact(self, contact):
+        self.contacts.append(contact)
+        return (f'Contact: {contact.name} phone: {contact.phone} added.')
+    def all_contacts(self):
+        for i in self.contacts: 
+            print(f'{i}')
+    def get_phone(self, name):
+        result = 'Phone not found'
+        for i in self.contacts:
+            if i.name == name: 
+                result = f'Contact {i.name} phone is {i.phone}'
+        return result
+    def change_contact(self, name, phone):
+        result = 'Phone not found'
+        for i in self.contacts:
+            if i.name == name: 
+                i.phone = phone
+                result = f'Contact updated to {i}'
+        return result
+
+
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+    
+def main():
+    contacts = ContactList()
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        match command:
+            case "close" | "exit":
+                print("Good bye!")
+                break
+            case "hello":
+                print("How can I help you?")
+            case "add":
+                contact = Contact(args[0], args[1])
+                print(contacts.add_contact(contact))
+            case "change":                
+                print(contacts.change_contact(args[0], args[1]))
+            case "phone":
+                print(contacts.get_phone(args[0]))
+            case "all":
+                contacts.all_contacts()
+            case _:
+                print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
+
